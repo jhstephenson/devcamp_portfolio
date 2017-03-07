ddbd4d7d467d53ca986c8b50b8ca2e2cf9883ccf
James Stephenson <jstephen@scc-i.com>
2017-03-07 22:32:02 +0000

    Edit portfolios controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -22,12 +22,24 @@ class PortfoliosController < ApplicationController
   
   def edit
     @portfolio_item = Portfolio.find(params[:id])
-  
   end
+
+  def update
+    @portfolio_item = Portfolio.new(portfolio_item_params)
+    
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_item_params)
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
   
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_portfolio
+    def set_portfolio_item
       @portfolio_item = Portfolio.find(params[:id])
     end
 
